6726ae9c337c799ef0bf56580da80805150a8959
lleal24 <lorenaleal24@gmail.com>
2019-03-16 00:35:33 -0500

    Se implementa la clase BigDecimal para redondear y limitar los decimales del calculo de promedio de los tres numeros

--- build/generated/src/org/apache/jsp/promedio_jsp.java
+++ build/generated/src/org/apache/jsp/promedio_jsp.java
@@ -3,6 +3,8 @@ package org.apache.jsp;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.jsp.*;
+import java.math.RoundingMode;
+import java.math.BigDecimal;
 import java.net.URL;
 
 public final class promedio_jsp extends org.apache.jasper.runtime.HttpJspBase
@@ -42,39 +44,45 @@ public final class promedio_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write('\n');
- 
-   //Captura de la informacion
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+
+    //Captura de la informacion
     String numero1 = request.getParameter("num1");
     String numero2 = request.getParameter("num2");
     String numero3 = request.getParameter("num3");
-    
+
     //Convertir String en integer
-    double num1 =0;
-    double num2=0;
-    double num3=0;
-    double promedio=0;
+    double num1 = 0;
+    double num2 = 0;
+    double num3 = 0;
+    double promedio = 0;
     String url = "";
     
-    try{
-    num1=Double.parseDouble(numero1);
-    num2=Double.parseDouble(numero2);
-    num3=Double.parseDouble(numero3);
-    promedio = (num1+num2+num3)/3;
     
-    if (promedio <= 2.5){
-        url = "Imagenes/rojo.png";
-    }else if(promedio > 2.5 && promedio <= 3.5){
-        url = "Imagenes/amarillo.jpg";       
-    }else if (promedio > 3.5){
-        url = "Imagenes/verde.png";
-    }
+    try {
+        num1 = Double.parseDouble(numero1);
+        num2 = Double.parseDouble(numero2);
+        num3 = Double.parseDouble(numero3);
+        promedio = (num1 + num2 + num3) / 3;
         
-    }catch(Exception e){
-        out.print("FAIL");
-    
-}
-    
+
+        if (promedio <= 2.5) {
+            url = "Imagenes/rojo.png";
+        } else if (promedio > 2.5 && promedio <= 3.5) {
+            url = "Imagenes/amarillo.jpg";
+        } else if (promedio > 3.5) {
+            url = "Imagenes/verde.png";
+        }
+
+    } catch (Exception e) {
+        out.print("");
+
+    }
+    //clase que permite limitar el numero de decimales en una cifra y realizar redondeo.
+    BigDecimal promDecimal = new BigDecimal(promedio).setScale(2, RoundingMode.UP);
+
 
 
       out.write("\n");
@@ -87,26 +95,34 @@ public final class promedio_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("<html>\n");
       out.write("    <head>\n");
       out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">\n");
       out.write("        <title>Calculo de promedio</title>\n");
       out.write("    </head>\n");
       out.write("    <body>\n");
-      out.write("        <h1>Calcular Promedio</h1>\n");
-      out.write("        <!--Formulario-->\n");
-      out.write("        <form method=\"POST\" action=\"\">\n");
-      out.write("            <p>Ingrese primer numero: <input type=\"text\" name=\"num1\"/></p>\n");
-      out.write("            <p>Ingrese segundo numero: <input type=\"text\" name=\"num2\"/></p>\n");
-      out.write("            <p>Ingrese tercer numero: <input type=\"text\" name=\"num3\"/></p>\n");
-      out.write("            <p><input type=\"submit\" name=\"\" value=\"Calcular promedio\"/></p>\n");
-      out.write("            <p><b>Pomedio:</b> ");
-      out.print(promedio);
+      out.write("        <div class=\"formulario\">\n");
+      out.write("            <h1>Calcular Promedio</h1>\n");
+      out.write("            <!--Formulario-->\n");
+      out.write("            <form method=\"POST\" action=\"\">\n");
+      out.write("                <label>Primer numero</label>\n");
+      out.write("                <input type=\"text\" name=\"num1\" placeholder=\"ingrese dato\" autofocus /></br>\n");
+      out.write("                <label>Segundo numero</label>\n");
+      out.write("                <input type=\"text\" name=\"num2\" placeholder=\"ingrese dato\"/></br>\n");
+      out.write("                <label>Tercer numero</label>\n");
+      out.write("                <input type=\"text\" name=\"num3\" placeholder=\"ingrese dato\"/></br>\n");
+      out.write("                <input type=\"submit\" name=\"\" value=\"Calcular promedio\" class=\"button\"/>\n");
+      out.write("                <p><b>Pomedio:</b> ");
+      out.print(promDecimal.doubleValue());
       out.write("</p>               \n");
-      out.write("        </form>\n");
-      out.write("            <img src=\"");
+      out.write("            </form>\n");
+      out.write("        </div>\n");
+      out.write("            <div class=\"formulario\" >\n");
+      out.write("             <img src=\"");
       out.print(url);
       out.write("\" width=\"200\" height=\"200\"/>\n");
-      out.write("      \n");
-      out.write("        \n");
-      out.write("        \n");
+      out.write("        </div>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
       out.write("    </body>\n");
       out.write("</html>\n");
     } catch (Throwable t) {
--- build/web/promedio.jsp
+++ build/web/promedio.jsp
@@ -1,36 +1,42 @@
+<%@page import="java.math.RoundingMode"%>
+<%@page import="java.math.BigDecimal"%>
 <%@page import="java.net.URL"%>
-<% 
-   //Captura de la informacion
+<%
+    //Captura de la informacion
     String numero1 = request.getParameter("num1");
     String numero2 = request.getParameter("num2");
     String numero3 = request.getParameter("num3");
-    
+
     //Convertir String en integer
-    double num1 =0;
-    double num2=0;
-    double num3=0;
-    double promedio=0;
+    double num1 = 0;
+    double num2 = 0;
+    double num3 = 0;
+    double promedio = 0;
     String url = "";
     
-    try{
-    num1=Double.parseDouble(numero1);
-    num2=Double.parseDouble(numero2);
-    num3=Double.parseDouble(numero3);
-    promedio = (num1+num2+num3)/3;
     
-    if (promedio <= 2.5){
-        url = "Imagenes/rojo.png";
-    }else if(promedio > 2.5 && promedio <= 3.5){
-        url = "Imagenes/amarillo.jpg";       
-    }else if (promedio > 3.5){
-        url = "Imagenes/verde.png";
-    }
+    try {
+        num1 = Double.parseDouble(numero1);
+        num2 = Double.parseDouble(numero2);
+        num3 = Double.parseDouble(numero3);
+        promedio = (num1 + num2 + num3) / 3;
         
-    }catch(Exception e){
-        out.print("FAIL");
-    
-}
-    
+
+        if (promedio <= 2.5) {
+            url = "Imagenes/rojo.png";
+        } else if (promedio > 2.5 && promedio <= 3.5) {
+            url = "Imagenes/amarillo.jpg";
+        } else if (promedio > 3.5) {
+            url = "Imagenes/verde.png";
+        }
+
+    } catch (Exception e) {
+        out.print("");
+
+    }
+    //clase que permite limitar el numero de decimales en una cifra y realizar redondeo.
+    BigDecimal promDecimal = new BigDecimal(promedio).setScale(2, RoundingMode.UP);
+
 
 %>
 
@@ -42,21 +48,29 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
         <title>Calculo de promedio</title>
     </head>
     <body>
-        <h1>Calcular Promedio</h1>
-        <!--Formulario-->
-        <form method="POST" action="">
-            <p>Ingrese primer numero: <input type="text" name="num1"/></p>
-            <p>Ingrese segundo numero: <input type="text" name="num2"/></p>
-            <p>Ingrese tercer numero: <input type="text" name="num3"/></p>
-            <p><input type="submit" name="" value="Calcular promedio"/></p>
-            <p><b>Pomedio:</b> <%=promedio%></p>               
-        </form>
-            <img src="<%=url%>" width="200" height="200"/>
-      
-        
-        
+        <div class="formulario">
+            <h1>Calcular Promedio</h1>
+            <!--Formulario-->
+            <form method="POST" action="">
+                <label>Primer numero</label>
+                <input type="text" name="num1" placeholder="ingrese dato" autofocus /></br>
+                <label>Segundo numero</label>
+                <input type="text" name="num2" placeholder="ingrese dato"/></br>
+                <label>Tercer numero</label>
+                <input type="text" name="num3" placeholder="ingrese dato"/></br>
+                <input type="submit" name="" value="Calcular promedio" class="button"/>
+                <p><b>Pomedio:</b> <%=promDecimal.doubleValue()%></p>               
+            </form>
+        </div>
+            <div class="formulario" >
+             <img src="<%=url%>" width="200" height="200"/>
+        </div>
+
+
+
     </body>
 </html>
